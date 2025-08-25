2307b66fed4d5566a89a304395af44a87d8157fa
Jairo Mendes <jairo2998@gmail.com>
2025-08-25 13:28:49 -0300

    feat: add 10 new categories and expand word list

---
+++ src/data/words.js
@@ -0,0 +1,12 @@
+export const wordList = {
+    carro: ["Motor", "Porta", "Volante", "Pneu", "Farol", "Capô", "Roda", "Freio", "Banco", "Cinto"],
+    fruta: ["Banana", "Mamão", "Maçã", "Laranja", "Uva", "Morango", "Abacaxi", "Melancia", "Pêssego", "Limão"],
+    computador: ["Monitor", "Teclado", "Mouse", "CPU", "Memória", "HD", "SSD", "Placa de Vídeo", "Processador", "Fonte"],
+    animal: ["Cachorro", "Gato", "Elefante", "Leão", "Girafa", "Tigre", "Macaco", "Zebra", "Urso", "Panda"],
+    profissão: ["Médico", "Professor", "Engenheiro", "Advogado", "Cozinheiro", "Bombeiro", "Policial", "Programador", "Arquiteto", "Enfermeiro"],
+    país: ["Brasil", "Argentina", "Canadá", "Japão", "Austrália", "França", "Itália", "Espanha", "México", "China"],
+    esporte: ["Futebol", "Basquete", "Vôlei", "Natação", "Tênis", "Ciclismo", "Boxe", "Judô", "Golfe", "Atletismo"],
+    cor: ["Vermelho", "Azul", "Verde", "Amarelo", "Roxo", "Laranja", "Preto", "Branco", "Marrom", "Rosa"],
+    instrumento: ["Violão", "Piano", "Guitarra", "Bateria", "Flauta", "Saxofone", "Trompete", "Violino", "Harpa", "Acordeon"],
+    filme: ["Titanic", "Avatar", "Star Wars", "Harry Potter", "Jurassic Park", "Matrix", "Forrest Gump", "Pulp Fiction", "Gladiador", "Toy Story"]
+};
