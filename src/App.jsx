57a761f34f1bb2be09435f5df1ce23c29b9adfcf
Jairo Mendes <jairo2998@gmail.com>
2025-08-27 21:16:17 -0300

    Styles and initial screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+
+import StartScreen from './components/StartScreen'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h2>Secret Word</h2>
+      <div className='App'>
+        <StartScreen />
       </div>      
     </>
   )
