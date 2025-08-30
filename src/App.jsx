eca8d6d6efd807d02e3cdc1e52bd59197b02592a
Jairo Mendes <jairo2998@gmail.com>
2025-08-30 11:46:33 -0300

    game components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import { useState } from 'react'
 
+// CSS
 import './App.css'
 
-import StartScreen from './components/StartScreen'
+// Rect
+import { useCallback, useEffect, useState } from 'react'
+
+//Data
+import {wordList} from "./data/words";
+
+// components
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"},
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [gameStage, setGameSatge] = useState(stages[0].name);
+  const [words] = useState(wordList)
+
+  console.log(words);
+  
 
   return (
-    <>
-      <div className='App'>
-        <StartScreen />
-      </div>      
-    </>
+    
+    <div className='App'>
+      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <Game />}
+      {gameStage === 'end' && <GameOver />}
+    </div>      
+    
   )
 }
 
